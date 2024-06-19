50c5c87af0822ae7b41e1363aca084146fd0e157
Florian Chaffard <fchaffard@usm.cl>
2024-06-19 14:42:56 -0400

    adding management of env variable

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,13 @@
 // index.ts
+import { config } from 'dotenv';
+import path from 'path';
+
+// Ensure dotenv reads the correct .env file
+config({ path: path.resolve(__dirname, '../.env') });
+
 import app from './routes/userRoutes';
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
+    console.log('DATABASE_URL:', process.env.DATABASE_URL);
 });
