8a7b2dd57b419a427387ff24fbd8effb4c42e0b6
Luna Schenk <schenk.luna@gmail.com>
2024-06-22 18:37:35 -0400

    rm print of env

--- api/src/index.ts
+++ api/src/index.ts
@@ -10,5 +10,4 @@ import app from './routes/userRoutes';
 // Start the server on port 3000
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
-    console.log('DATABASE_URL:', process.env.DATABASE_URL);
 });
